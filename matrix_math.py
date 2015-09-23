3b9af853a26adf5a13bbee9d57c51f443337cd8a
zseaborn63 <zseaobnr63@gmail.com>
2015-09-23 14:28:43 -0400

    forgot to save in Atom.  This is the real working Matrix class

--- matrix_math.py
+++ matrix_math.py
@@ -56,3 +56,70 @@ class Vector:
             output_num = self.vector[i] * scalar
             output_vector.append(output_num)
         return output_vector
+
+class Matrix(Vector):
+
+    def __init__(self, matrix):
+        self.matrix = matrix
+        for row in self.matrix:
+            self.row = row
+
+    def __str__(self):
+        return "{}".format(self.matrix)
+
+    @property
+    def shape(self):
+        for row in self.matrix:
+            num_rows = len(self.matrix)
+            for col in row:
+                num_col = len(row)
+        return num_rows, num_col
+
+    def shape_checker(self, other):
+        if self.shape[1] != other.shape[0]:
+            raise ValueError("Shape Rule Violated!")
+
+    def __add__(self, other):
+        if self.shape != other.shape:
+            raise ValueError("Shape Rule Violated!")
+        output_matrix = []
+        for self.row in self.matrix:
+            output_row = []
+            for x in range(len(self.row)):
+                output_num = self.row[x] + other.row[x]
+                output_row.append(output_num)
+            output_matrix.append(output_row)
+        return output_matrix
+
+    def __sub__(self, other):
+        if self.shape != other.shape:
+            raise ValueError("Shape Rule Violated!")
+        output_matrix = []
+        for self.row in self.matrix:
+            output_row = []
+            for x in range(len(self.row)):
+                output_num = self.row[x] - other.row[x]
+                output_row.append(output_num)
+            output_matrix.append(output_row)
+        return output_matrix
+
+    def __mul__(self, other):
+        if type(other) == int:
+            output_matrix = []
+            for self.row in self.matrix:
+                output_row = []
+                for column in self.row:
+                    calculated_value = column * other
+                    output_row.append(calculated_value)
+                output_matrix.append(output_row)
+            return output_matrix
+        else:
+            other = Vector(other)
+            #self.shape_checker(other)
+            final_vec = []
+            for self.row in self.matrix:
+                placeholder = 0
+                for x in range(len(self.row)):
+                    placeholder += self.row[x] * other[x]
+                final_vec.append(placeholder)
+            return final_vec
